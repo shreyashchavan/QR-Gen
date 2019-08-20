3f0f69f315f37c0f44c7bf522eaa03877fe3bcac
Shreyash Chavan <54321675+shreyashchavan@users.noreply.github.com>
2019-08-20 20:39:29 +0530

    just simple change in o/p.

--- qrcode.py
+++ qrcode.py
@@ -1,6 +1,6 @@
 import pyqrcode 
 from pyqrcode import QRCode  
-s = print('input your website name : ')
+s = print('input your Website link or Prpfile URL : ')
 use = input()
 url = pyqrcode.create(use)  
 url.svg("myqr.svg", scale = 8)
